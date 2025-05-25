d64ccccca6b249e8a060ba8f98475b30f993e046
ththune <135433132+ththune@users.noreply.github.com>
2025-05-25 12:49:05 +0200

    Put helper methods into BoardLogic.cs

--- BlazorGame/GameLogic/BoardLogic.cs
+++ BlazorGame/GameLogic/BoardLogic.cs
@@ -19,4 +19,46 @@ public class BoardLogic
         Rows = rows;
         Columns = columns;
     }
+
+
+    /// <summary>
+    ///  Gets a list of empty cells on the board that are not occupied by the snake.
+    /// </summary>
+    /// <param name="snakeBody"></param>
+    /// <returns></returns>
+    public List<(int Row, int Column)> GetEmptyCells(LinkedList<(int Row, int Column)> snakeBody)
+    {
+        var emptyCells = new List<(int rows, int columns)>();
+        for (var row = 0; row < Rows; row++)
+        {
+            for (var column = 0; column < Columns; column++)
+            {
+                if (!IsSnakeCell(row, column, snakeBody)) emptyCells.Add((row, column));
+            }
+        }
+
+        return emptyCells;
+    }
+
+    //TODO: Should use Position intstead of (row, column)...
+    /// <summary>
+    ///  Check if the snake occupies the specified cell.
+    /// </summary>
+    /// <param name="row"></param>
+    /// <param name="column"></param>
+    /// <param name="snakeBody"></param>
+    /// <returns></returns>
+    public static bool IsSnakeCell(int row, int column, IEnumerable<(int Row, int Column)> snakeBody) =>
+        snakeBody.Any(cell => cell.Row == row && cell.Column == column);
+
+
+    /// <summary>
+    /// Check if food occupies the specified cell.
+    /// </summary>
+    /// <param name="row"></param>
+    /// <param name="column"></param>
+    /// <param name="foodPosition"></param>
+    /// <returns></returns>
+    public static bool IsFoodCell(int row, int column, (int Row, int Column) foodPosition) =>
+        foodPosition.Row == row && foodPosition.Column == column;
 }
